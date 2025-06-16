3b1a95bc2b93e5853158bece6a8492a9e4bad31a
Alex Sigalos <alex.wayne.dev@gmail.com>
2025-06-16 01:55:19 -0700

    Feat: Can force UI update by restarting explorer.

--- ChangeTheme.ps1
+++ ChangeTheme.ps1
@@ -4,9 +4,10 @@ Toggles or sets the Windows theme between light and dark modes.
 
 .DESCRIPTION
 This script changes the Windows system theme either by:
-- Toggling between light/dark modes automatically
-- Setting a specific mode (light/dark)
-- Applying a custom .theme file
+    - Toggling between light/dark modes automatically
+    - Setting a specific mode (light/dark).
+    - Applying a custom .theme file.
+Allows forcing UI to update (restart explorer).
 
 .PARAMETER Theme
 Specify a custom theme file to apply. Including the .theme extension is optional.
@@ -15,6 +16,9 @@ This parameter overrides the Mode parameter.
 .PARAMETER Mode
 Specify whether to switch to light or dark mode.
 Valid values: 'light', 'dark'
+
+.PARAMETER Force
+Specify whether to force the UI to update by restarting explorer. No value necessary.
 
 .EXAMPLE
 PS> .\ChangeTheme.ps1
@@ -35,6 +39,10 @@ Applies the specified custom theme.
 .EXAMPLE
 PS> .\ChangeTheme.ps1 -t "MyCustomTheme"
 Applies the specified custom theme.
+
+.EXAMPLE
+PS> .\ChangeTheme.ps1 -Mode dark -Force
+Sets dark mode and restarts Explorer to ensure changes apply immediately
 
 .EXAMPLE
 PS> .\ChangeTheme.ps1 -Help
@@ -58,13 +66,14 @@ Author: Alex Sigalos
 Date Created: June 16, 2025
 #>
 
-[CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact='Medium')]
+[CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
 param (
     [Alias('t')]
     [string]$Theme,
     [Alias('m')]
     [ValidateSet("light", "dark")]
     [string]$Mode,
+    [switch]$Force,
     [Alias('?', 'h')]
     [switch]$Help
 
@@ -132,4 +141,15 @@ if ($PSCmdlet.ShouldProcess("$themeFile", "Apply theme")) {
     else {
         Write-Host "Successfully applied $themeApplied theme" -ForegroundColor Green
     }
+
+    if ($Force) {
+        if ($PSCmdlet.ShouldProcess("Explorer Process", "Restart")) {
+            Write-Verbose "Restarting Explorer process..." -Verbose
+            Stop-Process -Name explorer -Force -ErrorAction SilentlyContinue
+            Start-Process explorer
+            Start-Sleep -Seconds 1
+            Write-Host "Explorer restarted to apply theme changes" -ForegroundColor Yellow
+        }
+    }
 }
+
